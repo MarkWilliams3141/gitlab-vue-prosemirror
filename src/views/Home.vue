8a3c97a24f8667e5b2fde46ed92788dac224cd90
Mark Williams <m.williams.3141@gmail.com>
2019-10-02 14:34:27 +0100

    Initial setup with editor setup.

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <Editor dev-mode component-id="myComponent" value='{
+      "type":"doc",
+      "content":[
+         {
+            "type":"paragraph",
+            "content":[
+               {
+                  "type":"text",
+                  "text":"This is example data."
+               }
+            ]
+         },
+         {
+            "type":"paragraph",
+            "content":[
+               {
+                  "type":"text",
+                  "text":"To demo how content can be loaded with schema conforming json."
+               }
+            ]
+         }
+      ]
+   }' evidence headings lists xrefs references targetPublications devTools/>
+  </div>
+</template>
+
+<script>
+import Editor from 'rpharms-vue-editor'
+
+export default {
+  name: 'home',
+  components: {
+    Editor
+  }
+}
+</script>
